30076b10e3c9c39bb9d3e11ddb780e2b9b386aae
Jaime Cuebas <jimbosoccer28@gmail.com>
2024-10-22 17:19:45 -0400

    Update index.js to add Hello World and Greeting functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,18 @@
 /**
  * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Hello World function
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Greeting function
+exports.greetUser = onRequest((request, response) => {
+  const name = request.query.name || "Guest";
+  logger.info(`Greeting request received for: ${name}`, {structuredData: true});
+  response.send(`Hello, ${name}!`);
+});
