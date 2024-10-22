4600ab5f02cd2a4c3e84e0538b391934ce0deb57
Jaime Cuebas <jimbosoccer28@gmail.com>
2024-10-22 18:12:07 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,28 @@
  * Import function triggers from their respective submodules:
  */
 const {onRequest} = require("firebase-functions/v2/https");
+const cors = require("cors"); // Import CORS
 const logger = require("firebase-functions/logger");
+
+// Initialize CORS
+const corsOptions = {
+  origin: true, // Allow all origins
+};
 
 // Hello World function
 exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  cors(corsOptions)(request, response, () => {
+    logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+  });
 });
 
 // Greeting function
 exports.greetUser = onRequest((request, response) => {
-  const name = request.query.name || "Guest";
-  logger.info(`Greeting request received for: ${name}`, {structuredData: true});
-  response.send(`Hello, ${name}!`);
+  cors(corsOptions)(request, response, () => {
+    const name = request.query.name || "Guest";
+    logger.info(`Greeting request received for: ${name}`,
+        {structuredData: true});
+    response.send(`Hello, ${name}!`);
+  });
 });
